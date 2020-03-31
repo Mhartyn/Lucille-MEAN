defc6c4d09f700cf54f283db24c55477b969ad6c
mhartyn <mhartynpaez@hotmail.com>
2020-03-30 23:52:11 -0500

    mongoose typeScript

--- src/server/entidades/base.ts
+++
@@ -1,6 +0,0 @@
-/* class Base{
-    this.UsuarioCreacion: any; { type: mongoose.Types.ObjectId, ref: "Usuario", required: true },
-    FechaCreacion: { type: Date, required: true, default: Date.now },
-    UsuarioModificacion: { type: mongoose.Types.ObjectId, ref: "Usuario", required: false },
-    FechaModificacion: { type: Date, required: false }   
-} */
