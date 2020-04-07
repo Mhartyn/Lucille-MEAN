058300ae822c4b250ebcdeaac4eeed8766553444
mhartyn <mhartynpaez@hotmail.com>
2020-04-07 15:41:52 -0500

    orden y direccion en respuesta de consulta paginada

--- src/server/modelo/interfaces/genericos/iRespuestaPaginada.ts
+++ src/server/modelo/interfaces/genericos/iRespuestaPaginada.ts
@@ -4,6 +4,8 @@ import { IConsultaModel } from './iConsulta';
 interface IRespuestaPaginada{
     items: any;
     pagina: number;
+    orden: string;
+    direccion: number;
     tamanioPagina: number;
     totalItems: number;
     totalPaginas: number;
@@ -12,6 +14,8 @@ interface IRespuestaPaginada{
 export default class RespuestaPaginada<T> implements IRespuestaPaginada{
     items: any;
     pagina: number;
+    orden: string;
+    direccion: number;
     tamanioPagina: number;
     totalItems: number;
     totalPaginas: number;
@@ -19,6 +23,8 @@ export default class RespuestaPaginada<T> implements IRespuestaPaginada{
     constructor(criterio: IConsultaModel<T>, res: any, total: number){
         this.items= res;
         this.pagina= criterio.pagina;
+        this.orden= criterio.orden;
+        this.direccion= criterio.direccion;
         this.tamanioPagina= criterio.tamanio;
         this.totalItems= total;
         this.totalPaginas= utils.numeroPaginas(total, criterio.tamanio);        
