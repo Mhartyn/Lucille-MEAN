cf663c11dc6cfadc1c1bc76fb158358b8cec2e19
mhartyn <mhartynpaez@hotmail.com>
2020-07-30 02:35:00 -0500

    prueba con jenkins

---
+++ jenkins/deliver.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+echo 'The following "npm" command builds your Node.js/React application for'
+echo 'production in the local "build" directory (i.e. within the'
+echo 'the best performance.'
+set -x
+npm run build
+set +x
+
+echo 'The following "npm" command runs your Node.js/React application in'
+echo 'development mode and makes the application available for web browsing.'
+echo 'The "npm start" command has a trailing ampersand so that the command runs'
+echo 'as a background process (i.e. asynchronously). Otherwise, this command'
+echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
+set -x
+npm start
+set +x
